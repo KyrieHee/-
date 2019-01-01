e8bc46da45add4631b8f0909058a2fd2b409407f
HeYun <hyf6127@126.com>
2019-01-01 23:57:27 +0800

    Update 001.cpp

--- 1_array/001.cpp
+++ 1_array/001.cpp
@@ -3,28 +3,33 @@ public:
     bool Find(int target, vector<vector<int> > array) {
         int row = (int)array.size();
         int col = (int)array[0].size();
+        //判断数组是否含有元素
         if (row == 0 || col == 0)
             return false;
+        //判断下标是否越界
         if (target < array[0][0] || target > array[row - 1][col - 1])
             return false;
+        
+        
         int i = 0;
         int j = col - 1;
+        //在数组范围内查找
         while(i < row && j >= 0)
         {
-            if (array[i][j] > target)
+            if (target < array[i][j])
             {
-                j--;
+                j--; //查找的元素较少，往上找
             }
-            else if(array[i][j] < target)
+            else if(target > array[i][j])
             {
-                i++;
+                i++; //查找的元素较大，往右找
             }
             else
             {
-                return 1;
+                return 1; //找到元素
             }
         }
-    return 0;
+        return 0;
     }
 };
  
