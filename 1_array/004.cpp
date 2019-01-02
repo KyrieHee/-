de911dfc7fa505311d4920570f48e088856a8565
HeYun <hyf6127@126.com>
2019-01-02 12:39:06 +0800

    Update 004.cpp

--- 1_array/004.cpp
+++ 1_array/004.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<assert.h>
 
 using namespace std;
 
@@ -27,64 +26,83 @@ class String
 //普通构造函数
 String::String(const char *str)
 {
-  if(str == NULL)
-  {
-
-    m_data = new char[1];
-    assert(m_data != NULL);
-    *m_data = '\0';
-  }
-  else
-  {
-    int length = strlen(str);
-    m_data = new char[length+1];
-    strcpy(m_data, str);
-  }
+	cout<<"Constructing Function"<<endl;
+	//若str为空，存空字符串""
+	if(str == NULL)
+	{
+		//分配一个字节
+		m_data = new char[1];
+		//将其赋值为字符串结束符
+		*m_data = '\0';
+	}
+	else
+	{
+		int len = strlen(str);
+		//分配空间
+		m_data = new char[len+1];
+		//拷贝到m_data
+		strcpy(m_data, str);
+	}
 }
 
 
 //拷贝构造函数
 String::String(const String &other)
 {
-  int length = strlen(other.m_data);
-  m_data = new char[length+1];
-  strcpy(m_data, other.m_data);
+	cout<<"Constructing Copy Function"<<endl;
+	int len = strlen(other.m_data);
+	//分配空间
+	m_data = new char[len+1];
+	//拷贝到m_data
+	strcpy(m_data, other.m_data);
 }
 
 
 //析构函数
 String::~String(void)
 {
-  delete [] m_data;
-  m_data = NULL;
+	cout<<"Destructing Function"<<endl;
+	//若m_data不为空，释放堆内存
+	if(m_data != NULL)
+	{
+		delete [] m_data;
+		//释放后将m_data置为NULL，防止出现野指针。
+		m_data = NULL;
+	}
+	
 }
 
 
 //赋值函数
 String & String::operator=(const String & other)
 {
-  if(this == &other)
-    return *this;
-    
-  delete[] m_data;
-  m_data = NULL;
+	cout<<"Operator = Function."<<endl;
+	//若对象和other是同一个对象直接返回
+	if(this == &other)
+	{
+		return *this;
+	} 
+
+	//释放堆内存
+	delete[] m_data;
+	m_data = NULL;
   
-  int length = strlen(other.m_data);
-  m_data = new char[length+1];
-  assert(m_data != NULL);
-  strcpy(m_data, other.m_data);
-  
-  return *this;
+	int len = strlen(other.m_data);
+	//分配空间
+    m_data = new char[len+1];
+	//拷贝到m_data
+	strcpy(m_data, other.m_data);
 }
 
 //main函数
 int main(char argc, char *argv)
 {
-  String s1; //普通构造函数
-  String s2("hello CPP");//拷贝构造函数
-  s1 = s2;//赋值函数
-  
-  return 0;
+	String s1; //普通构造函数
+	String s3(s1); //拷贝构造函数
+	String s2("hello CPP");//普通构造函数
+	s1 = s2;//赋值函数
+	system("pause");
+	return 0;
 }
 
 
