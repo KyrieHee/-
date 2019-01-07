0d00cb22e63f4261b01fc790874abb28362e2c47
HeYun <hyf6127@126.com>
2019-01-07 21:03:23 +0800

    Update 003.cpp

--- 2_String/003.cpp
+++ 2_String/003.cpp
@@ -1,1 +1,44 @@
+/*
+实现 strStr() 函数。
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1:
+输入: haystack = "hello", needle = "ll"
+输出: 2
+
+示例 2:
+输入: haystack = "aaaaa", needle = "bba"
+输出: -1
+*/
+
+
+/*
+params:
+  haystack -- 要被检索的 C 字符串.
+  needle -- 在 haystack 字符串内要搜索的小字符串.  
+ 
+return:
+  该函数返回在 haystack 中第一次出现 needle 字符串的位置，如果未找到则返回 null。
+*/
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int m = haystack.length(), n = needle.length();
+        if (!n) {
+            return 0;
+        }
+        for (int i = 0; i < m - n + 1; i++) {
+            int j = 0;
+            for (; j < n; j++) {
+                if (haystack[i + j] != needle[j]) {
+                    break;
+                }
+            }
+            if (j == n) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
 
