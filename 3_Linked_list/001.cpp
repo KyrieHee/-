1298dd44764e0fc5f0fb89fcc305f3afead65b78
HeYun <hyf6127@126.com>
2019-01-07 21:35:23 +0800

    Update 001.cpp

--- 3_Linked_list/001.cpp
+++ 3_Linked_list/001.cpp
@@ -17,9 +17,38 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        //判断链表是否为空
+        if(head == NULL || head->next == NULL)
+            return head;
         
+        ListNode *p = head;
+        ListNode *q = p->next;
+        ListNode *r;
+        
+        p->next = NULL;
+        while(q != NULL)
+        {
+            r = q->next;
+            q->next = p;
+            p = q;
+            q = r;
+        }
+        return p;
+          
     }
 };
