a0d88e199626a657bca10a290b029ea95394be39
HeYun <hyf6127@126.com>
2019-01-07 17:34:32 +0800

    Update 001.cpp

--- 2_String/001.cpp
+++ 2_String/001.cpp
@@ -76,9 +76,30 @@ public:
 
 //C版本
 int myAtoi(char* str) {
-    if(str==NULL)
+    
+    int i = 0, sign = 1, base = 0, n = strlen(str);
+    
+    
+    if(n == 0)
         return 0;
     
+    while (i < n && str[i] == ' ') 
+        ++i;
+    
+    if (str[i] == '+' || str[i] == '-') 
+    {
+        sign = (str[i++] == '+') ? 1 : -1;
+    }
+
+    while (i < n && str[i] >= '0' && str[i] <= '9') 
+    {
+        if (base > INT_MAX / 10 || (base == INT_MAX / 10 && str[i] - '0' > 7)) 
+        {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        base = 10 * base + (str[i++] - '0');
+    }        
+    return base * sign;
     
     
 }
